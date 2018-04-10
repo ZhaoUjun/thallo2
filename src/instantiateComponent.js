a6c019a06e077779147518b4e23450597d855b8d
ujun <zhaorujunfor@tianzehuitong.com>
2018-04-10 14:48:39 +0800

    modify spelling

--- src/instantiateComponent.js
+++ src/instantiateComponent.js
@@ -1,10 +1,10 @@
-import { CompositeComponnet } from './CompositeComponent'
+import { CompositeComponent } from './CompositeComponent'
 import { DomComponent } from './DomComponent'
 
 export function instantiateComponent (element){
     const {type}=element;
     if (type==='function'){
-        return new CompositeComponnet(element)
+        return new CompositeComponent(element)
     }
     else if (type==='string'){
         return new DomComponent(element)
