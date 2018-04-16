9104a4b6fe5a63ef043103acb471652abe067597
rujun <zhaorujunfor@tianzehuitong.com>
2018-04-16 23:18:27 +0800

    debug

--- src/createDomNode.js
+++ src/createDomNode.js
@@ -3,8 +3,12 @@ import {NODE_TAG} from './constant'
 export function createDomNode (vNode,parentContext,parentComponent,isSvg=false){
     const {tag}=vNode;
     let domNode;
-    if(tag===NODE_TAG){
+    if(tag & (NODE_TAG.NORMAL_COMPONENT | NODE_TAG.STATELESS)){
         domNode=vNode.mount(parentContext,parentComponent)
+    }
+    else if (tag & NODE_TAG.NODE) {
+        return 
     }
     return domNode
 }
+
