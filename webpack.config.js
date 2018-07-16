55739df60f339a3217850f12be84ddcaf9c7acbc
rujun <zhaorujunfor@tianzehuitong.com>
2018-07-16 22:52:09 +0800

    change test entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,48 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
-  module.exports = {
-    mode:'development',
-    entry: {
-      React: './src/index.js',
-    },
-    resolve:{
-      alias:{
-        react:path.resolve(__dirname+'src/React')
-      }
-    },
-    devtool: 'cheap-module-source-map',
-    devServer: {
-     contentBase: './dist'
-    },
-    plugins: [
-      new CleanWebpackPlugin(['dist']),
-      new HtmlWebpackPlugin({
-        title: 'Development',
-        template:'./test/index.html'
-        
-      })
-    ],
-    output: {
-      filename: 'React.js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/'
-    },
-    module:{
-        rules:[
-            {
-                test: /\.(js|jsx)$/,
-                include:path.resolve(__dirname, 'src'),
-                loader: require.resolve('babel-loader'),
-                options: {
-                    presets: ["env", "react"],
-                    plugins: [
-                        "transform-react-jsx",
-                        "transform-class-properties",
-                        "transform-object-rest-spread"
-                    ]
-                },
-            }
-        ]
-    }
-  };
+module.exports = {
+	mode: "development",
+	entry: {
+		React: "./test/test.js"
+	},
+	resolve: {
+		alias: {
+			react: path.resolve(__dirname, "src/React")
+		}
+	},
+	devtool: "cheap-module-source-map",
+	devServer: {
+		contentBase: "./dist"
+	},
+	plugins: [
+		new CleanWebpackPlugin(["dist"]),
+		new HtmlWebpackPlugin({
+			title: "Development",
+			template: "./test/index.html"
+		})
+	],
+	output: {
+		filename: "React.js",
+		path: path.resolve(__dirname, "dist"),
+		publicPath: "/"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				include: path.resolve(__dirname, "src"),
+				loader: require.resolve("babel-loader"),
+				options: {
+					presets: ["env", "react"],
+					plugins: [
+						"transform-react-jsx",
+						"transform-class-properties",
+						"transform-object-rest-spread"
+					]
+				}
+			}
+		]
+	}
+};
